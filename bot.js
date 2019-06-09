8ec13d62b08f71ab0f0bb2b145d9abb797ff9892
LeviWaiU <leongvincentwaiu@gmail.com>
2019-06-09 23:13:08 +0100

    Everything Except Auth

---
+++ bot.js
@@ -0,0 +1,32 @@
+var Discord = require('discord.io');
+var logger  = require('winston');
+var auth    = require('./auth.json');
+
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, { colorize: true});
+logger.level = 'debug';
+
+var bot = new Discord.Client({
+    token: auth.token,
+    autorun : true });
+
+bot.on('ready', function(evt) {
+    logger.info('Connected');
+    logger.info('Login as:' + bot.username + '-(' + bot.id + ')');
+});
+
+bot.on('message', function(user, userID, channelID, message, evt) {
+    if(message.substring(0,1) == "!") {
+        var args = message.substring(1).split('');
+        var cmd = args[0];
+        
+        args = args.splice(1);
+        switch(cmd){
+            default:
+                bot.sendMessage({
+                    to: channelID,
+                    message: "Sorry, I'm still a work in progress..."
+                });
+        }
+    }
+});
