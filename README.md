2fa5263fcce1c34b0ce473db8ad0ee16f467c6c6
LeviWaiU <leongvincentwaiu@gmail.com>
2019-06-10 13:40:27 +0100

    First Feature Prototype

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-##This is a readme.
+# This is a readme.
+
 The following is a bot that is written for a private server in discord. It includes generally vanilla code but may have influences and snippets from other existing solutions.
 
 This is written mainly as a programming exercise.
+
+## To Use
+
+Before use, please fill in the auth.json that is provided with this repository with your own client ID keys for discord, twitter, and any other API that you wish to use.
+
+Should you decide to not sue any of the features given by the API, all but the discord tokens are optional and would need to be filled on only if you want to use it.
+
+NOTE: Currently auth.json is not included. This would be fixed soon.
+
+## Current Feature List
+
+Says hello to you.
+(Implementing) Twitter user lookup and display of results into discord.
+
+## Other stuff.
+
+This is only written as a programming exercise and there are better off the shelf discord bots that serve the same purpose. I would only suggest you use this if you have a personal interest in my work or would want to use this as a framework for something bigger you are working on.
+
+The bot is currently in alpha and development is expected to continue. There are still many features that are missing.
+
+### Leong, Vincent Wai U
--- bot.js
+++ bot.js
@@ -2,13 +2,22 @@ var Discord = require('discord.io');
 var logger  = require('winston');
 var auth    = require('./auth.json');
 
+var Twitter = require('twitter');
+
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, { colorize: true});
 logger.level = 'debug';
 
 var bot = new Discord.Client({
-    token: auth.token,
+    token: auth.discord.token,
     autorun : true });
+
+var twitterClient = new Twitter({
+    consumer_key: auth.twitter.consumer_key,
+    consumer_secret: auth.twitter.consumer_secret,
+    access_token_key: auth.twitter.access_token_key,
+    access_token_secret: auth.twitter.access_token_secret
+});
 
 bot.on('ready', function(evt) {
     logger.info('Connected');
@@ -17,15 +26,54 @@ bot.on('ready', function(evt) {
 
 bot.on('message', function(user, userID, channelID, message, evt) {
     if(message.substring(0,1) == "!") {
-        var args = message.substring(1).split('');
+        var args = message.substring(1).split(' ');
         var cmd = args[0];
         
+    
         args = args.splice(1);
         switch(cmd){
+            
+            case 'help':
+                bot.sendMessage({
+                    to: channelID,
+                    message: "Here are the list of currently usable commands \n hello \n help"
+                });
+                break;
+            
+            case 'hello':
+                bot.sendMessage({
+                    to: channelID,
+                    message: "Hello! It's nice to see you!"
+                });
+            break;
+            
+            case 'twitter':
+                handle = args;
+                twitterClient.get('users/show', {screen_name : handle[0]}, function(error, tweet, response) {
+                    if(error) {
+                        if(error[0].code === 50){
+                            bot.sendMessage({
+                                to: channelID,
+                                message: error[0].message                                
+                            });
+                        } else {
+                        logger.info(error);
+                        throw error;
+                        };
+                    } else {
+                    user_url = "https://twitter.com/" + tweet.screen_name;
+                    bot.sendMessage({
+                        to: channelID,
+                        message: user_url
+                    });
+                    }
+                });
+            break;
+            
             default:
                 bot.sendMessage({
                     to: channelID,
-                    message: "Sorry, I'm still a work in progress..."
+                    message: "Sorry, I am still a work in progress and do not support that command yet"
                 });
         }
     }
